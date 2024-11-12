b2d4a419030bc387ca89e881f589a0d48c362410
_m1ster_uzbek_ <jakbarov4401@gmail.com>
2024-11-12 19:10:47 +0500

    curl.php

--- curl.php
+++ curl.php
@@ -1,13 +1,26 @@
 <?php
-class Currency{
+class Currency {
     const CURRENCY_API_URL = "https://cbu.uz/uz/arkhiv-kursov-valyut/json/";
-    public function __construct(){
+
+    public function fetchRates() {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, self::CURRENCY_API_URL);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $output = curl_exec($ch);
         curl_close($ch);
-        $decoded = json_decode($output);
-        echo $decoded[0]->Ccy;
+        $data = json_decode($output, true);
+
+        $filteredRates = array_map(function($item) {
+            return [
+                'Ccy' => $item['Ccy'],
+                'Rate' => $item['Rate']
+            ];
+        }, $data);
+
+
+        $filteredRates[] = ['Ccy' => 'UZS', 'Rate' => 1];
+
+        return $filteredRates;
     }
 }
+?>
